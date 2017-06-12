4969da214dd46d3bcdd992918e7aeeb46b4b9b85
Timothy Soehnlin <tim@eaiti.com>
2017-06-12 00:13:13 -0400

    Moving to SSL as this needs to be authenticated

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,15 @@
 import * as express from 'express';
 import { Pi } from './pi';
+import * as fs from 'fs';
+import * as https from 'https';
+
+let privateKey = fs.readFileSync('../cert/key.pem', 'utf-8')
+let certificate = fs.readFileSync('../cert/cert.pem', 'utf-8')
+
+let credentials = {
+  key: privateKey,
+  cert: certificate
+};
 
 Pi.init();
 
@@ -18,4 +28,4 @@ app.get('/camera/snapshot', async (req, res, next) => {
   await Pi.camera(req, res, false);
 });
 
-app.listen(80);
+https.createServer(credentials, app).listen(443);
