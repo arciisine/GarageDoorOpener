38225bf9903a5075ce7dd4f3d1ba8e314f0a708f
Timothy Soehnlin <tim@eaiti.com>
2017-11-22 19:53:55 -0500

    Timing is now server managed

--- native/app/app.component.ts
+++ native/app/app.component.ts
@@ -140,7 +140,7 @@ export class AppComponent implements OnInit, OnDestroy {
       await this.auth();
     }
 
-    await firebase.setValue('/', { [key]: { value: (value || true), time: Date.now() } });
+    await firebase.setValue('/', { [key]: { value: (value || true) } });
   }
 
   async activate() {
