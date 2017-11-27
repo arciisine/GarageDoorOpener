d8e904c764c1114ab5b28f20eb1c405f906e7017
Timothy Soehnlin <tim@eaiti.com>
2017-11-27 01:20:16 -0500

    Simplifying UI

--- native/app/app.component.ts
+++ native/app/app.component.ts
@@ -16,123 +16,129 @@ import { Telephony } from 'nativescript-telephony';
 import { SETTINGS } from './main-activity';
 
 @Component({
-    selector: "ns-app",
-    template: `
+  selector: "ns-app",
+  template: `
     <FlexboxLayout flexDirection="row" alignContent="center">
-        <Image #image (longPress)="activate()" stretch="aspectFill"></Image>
+        <Image [imageSource]="imageSource" (longPress)="activate()" stretch="aspectFill"></Image>
     </FlexboxLayout>
   `,
-    styles: [``]
+  styles: [``]
 })
 export class AppComponent implements OnInit, OnDestroy {
 
-    @ViewChild("image") image: ElementRef;
+  private ip = '192.168.1.168';
+  private appId = 'garagedoorapp-1d1fe.appspot.com';
 
-    private ip = '192.168.1.168';
-    private appId = 'garagedoorapp-1d1fe.appspot.com';
+  private user: any;
+  private lastSent = 0;
 
-    private content = require('./resources/image.html');
-    private user: any;
-    private lastSent = 0;
+  private imageSource: im.ImageSource;
+  private authPerm: Promise<any>;
+  private imageLoading: Promise<any>;
 
-    private authPerm: Promise<any>;
+  constructor(private _page: Page) {
+    this.resume = this.resume.bind(this);
+    this.loadSnapshot = this.loadSnapshot.bind(this);
+    _page.actionBarHidden = true;
+  }
 
-    constructor(private _page: Page) {
-        this.resume = this.resume.bind(this);
-        this.loadSnapshot = this.loadSnapshot.bind(this);
-        _page.actionBarHidden = true;
+  async ngOnInit() {
+    console.log('Initted');
+    app.on(app.resumeEvent, this.resume);
+    app.on(app.exitEvent, this.ngOnDestroy.bind(this));
+    this.resume();
+  }
+
+  async resume() {
+    console.log('Resumed');
+    this.loadSnapshot()
+
+    if (SETTINGS.voice) {
+      SETTINGS.voice = false;
+      this.activate();
     }
+  }
 
-    async ngOnInit() {
-        console.log('Initted');
-        app.on(app.resumeEvent, this.resume);
-        app.on(app.exitEvent, this.ngOnDestroy.bind(this));
-        this.resume();
+  async auth() {
+    if (!this.authPerm) {
+      this.authPerm = this._auth();
     }
+    return this.authPerm;
+  }
 
-    async resume() {
-        console.log('Resumed');
-        this.loadSnapshot()
+  async _auth() {
+    let deviceId = '';
+    let email = '';
 
-        if (SETTINGS.voice) {
-            SETTINGS.voice = false;
-            this.activate();
+    await Permissions.requestPermission((android as any).Manifest.permission.GET_ACCOUNTS, "Needed for auth")
+    let telephony = await Telephony();
+
+    let intent = android.accounts.AccountManager.newChooseAccountIntent(null, null, ['com.google'], false, null, null, null, null)
+    app.android.foregroundActivity.startActivityForResult(intent, 1);
+
+    let u = await new Promise((resolve, reject) => {
+      app.android.on(app.AndroidApplication.activityResultEvent, function (args: app.AndroidActivityResultEventData) {
+        if (args.requestCode === 1) {
+          let all = android.accounts.AccountManager.get(app.android.currentContext).getAccounts();
+          let primary = all[0];
+
+          firebase.login({
+            type: firebase.LoginType.PASSWORD,
+            passwordOptions: {
+              email: primary.name,
+              password: telephony.deviceId
+            }
+          }).then(resolve, reject);
+
         }
+      })
+    });
+    this.user = u;
+    // Clear out after use, so if needed again, it will not cache
+    setTimeout(() => this._auth = undefined, 1);
+  }
+
+  ngOnDestroy() {
+    app.off(app.resumeEvent, this.resume);
+  }
+
+  async loadSnapshot() {
+    if (!this.imageLoading) {
+      this.imageLoading = this._loadSnapshot();
+    }
+    return this.imageLoading;
+  }
+
+  async _loadSnapshot() {
+    while (true) {
+      console.log('Loading Image');
+      this.imageSource = await im.fromUrl(`https://storage.googleapis.com/${this.appId}/images/door-snap.jpg?cache=${Date.now()}`);
+      console.log('Loaded Image');
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    }
+  }
+
+  async sendMessage(key: string, value?: any) {
+    try {
+      await firebase.getCurrentUser();
+    } catch (e) {
+      await this.auth();
     }
 
-    async auth() {
-        if (!this.authPerm) {
-            this.authPerm = this._auth();
-        }
-        return this.authPerm;
+    await firebase.setValue('/', { [key]: { value: (value || true) } });
+  }
+
+  async activate() {
+    if (Date.now() < (this.lastSent + 1000)) { // Don't double tap
+      return;
     }
-
-    async _auth() {
-        let deviceId = '';
-        let email = '';
-
-        await Permissions.requestPermission((android as any).Manifest.permission.GET_ACCOUNTS, "Needed for auth")
-        let telephony = await Telephony();
-
-        let intent = android.accounts.AccountManager.newChooseAccountIntent(null, null, ['com.google'], false, null, null, null, null)
-        app.android.foregroundActivity.startActivityForResult(intent, 1);
-
-        let u = await new Promise((resolve, reject) => {
-            app.android.on(app.AndroidApplication.activityResultEvent, function (args: app.AndroidActivityResultEventData) {
-                if (args.requestCode === 1) {
-                    let all = android.accounts.AccountManager.get(app.android.currentContext).getAccounts();
-                    let primary = all[0];
-
-                    firebase.login({
-                        type: firebase.LoginType.PASSWORD,
-                        passwordOptions: {
-                            email: primary.name,
-                            password: telephony.deviceId
-                        }
-                    }).then(resolve, reject);
-
-                }
-            })
-        });
-        this.user = u;
-        // Clear out after use, so if needed again, it will not cache
-        setTimeout(() => this._auth = undefined, 1);
+    try {
+      await this.sendMessage('Activate');
+    } catch (e) {
+      // fallback if firebase is down
+      http.request({ url: `http://${this.ip}/activate`, method: 'POST' });
     }
-
-    ngOnDestroy() {
-        app.off(app.resumeEvent, this.resume);
-    }
-
-    async loadSnapshot() {
-        console.log('Loading Image');
-        let src = await im.fromUrl(`https://storage.googleapis.com/${this.appId}/images/door-snap.jpg?cache=${Date.now()}`);
-        console.log('Loaded Image');
-        let img = (this.image.nativeElement as Image);
-        img.imageSource = src;
-        setTimeout(this.loadSnapshot, 2000);
-    }
-
-    async sendMessage(key: string, value?: any) {
-        try {
-            await firebase.getCurrentUser();
-        } catch (e) {
-            await this.auth();
-        }
-
-        await firebase.setValue('/', { [key]: { value: (value || true) } });
-    }
-
-    async activate() {
-        if (Date.now() < (this.lastSent + 1000)) { // Don't double tap
-            return;
-        }
-        try {
-            await this.sendMessage('Activate');
-        } catch (e) {
-            // fallback if firebase is down
-            http.request({ url: `http://${this.ip}/activate`, method: 'POST' });
-        }
-    }
+  }
 }
 
 firebase.init();
