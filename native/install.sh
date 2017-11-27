f8a500a9e507a01ca01e99ee6126046c55748abf
Timothy Soehnlin <tim@eaiti.com>
2017-11-26 20:43:06 -0500

    Working on new builds

--- native/install.sh
+++ native/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-tns run android --release --key-store-path ./keystore.jks --key-store-password password --key-store-alias test --key-store-alias-password password --device 1
+#tns run android --release --key-store-path ./keystore.jks --key-store-password password --key-store-alias test --key-store-alias-password password --device 1
+npm run build-android-bundle --uglify --snapshot -- --release --key-store-path ./keystore.jks --key-store-password password --key-store-alias test --key-store-alias-password password
+adb install -r platforms/android/build/outputs/apk/GarageDoor-release.apk
