0aa5ef362e093a7c8abb695e95e95f8df06ff037
arciisine <timothy.soehnlin@gmail.com>
2025-12-28 21:33:04 -0500

    Moving away from rpio

--- server/resources/store-image.sh
+++ server/resources/store-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -n "Saving $1 ... "
-curl -XPOST localhost:9000/snapshot?img=$1 && echo 'success' || echo 'Failure'
+curl -XPOST localhost:3000/snapshot?img=$1 && echo 'success' || echo 'Failure'
 if [[ -e "$1" ]]; then
   rm $1
 fi
