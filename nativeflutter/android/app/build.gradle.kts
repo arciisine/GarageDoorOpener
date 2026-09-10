59ee6fcdce97e1b6f626f84637dda3e735dce339
arciisine <timothy.soehnlin@gmail.com>
2026-09-09 21:37:58 -0400

    Supporting ability to do fancy push notifications

--- nativeflutter/android/app/build.gradle.kts
+++ nativeflutter/android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -40,6 +41,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
